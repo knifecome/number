b876ff71cd9013d8b897436725d4348da72cdd47
Kevin <qhx717@qq.com>
2017-02-20 14:53:10 +0800

    Kevin新增number

---
+++ js/mediaquery2.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Kevin on 2017/2/16.
+ */
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';  /*大于750的也跟随变化*/
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
